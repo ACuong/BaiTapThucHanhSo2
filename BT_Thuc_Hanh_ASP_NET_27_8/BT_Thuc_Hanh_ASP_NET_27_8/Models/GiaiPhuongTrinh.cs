aa5e8feb89e4921fe04fd89d6862ac707ce19df9
Nguyen Van Cuong <1521050560@student.humg.edu.vn>
2021-08-31 13:36:11 +0700

    Giai PT Bac 1 done

--- BT_Thuc_Hanh_ASP_NET_27_8/BT_Thuc_Hanh_ASP_NET_27_8/Models/GiaiPhuongTrinh.cs
+++ BT_Thuc_Hanh_ASP_NET_27_8/BT_Thuc_Hanh_ASP_NET_27_8/Models/GiaiPhuongTrinh.cs
@@ -7,10 +7,11 @@ namespace BT_Thuc_Hanh_ASP_NET_27_8.Models
 {
     public class GiaiPhuongTrinh
     {
-        public double GiaiPTBac1 (double a, double b)
-    {
+        public double GiaiPTBac1(double a, double b)
+        {
             double x;
-            x = - b / a;
+            x = -b / a;
             return x;
+        }
     }
 }
